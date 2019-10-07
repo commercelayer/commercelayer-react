5c2f5082d2b2ed99fc60699ed398d20c4ad9649e
Alessandro Casazza <alessandro@commercelayer.io>
2019-10-07 19:19:44 +0200

    Fixed babeljs

--- rollup.config.js
+++ rollup.config.js
@@ -5,35 +5,33 @@ import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
-
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
-    svgr(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
-    }),
-    resolve(),
-    commonjs()
-  ]
+	input: 'src/index.js',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			sourcemap: true
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			sourcemap: true
+		}
+	],
+	plugins: [
+		external(),
+		postcss({
+			modules: true
+		}),
+		url(),
+		svgr(),
+		resolve(),
+		babel({
+			exclude: 'node_modules/**'
+		}),
+		commonjs()
+	]
 }
