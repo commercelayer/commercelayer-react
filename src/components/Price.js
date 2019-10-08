e4d3a6e811f3173f116409cb14077e662cd2b521
Alessandro Casazza <alessandro@commercelayer.io>
2019-10-08 17:06:35 +0200

    Added new AmountProps

--- src/components/Price.js
+++ src/components/Price.js
@@ -2,37 +2,55 @@
  * @class Price
  */
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 export default class Price extends Component {
-	shouldComponentUpdate() {
-		return false
-	}
+		shouldComponentUpdate() {
+				return false
+		}
 
-	static propTypes = {
-		ContainerTag: PropTypes.oneOf([ 'div', 'span' ]),
-		AmountTag: PropTypes.oneOf([ 'div', 'span' ]),
-		skuCode: PropTypes.string,
-		className: PropTypes.string
-	}
+		static propTypes = {
+				ContainerTag: PropTypes.oneOf(['div', 'span']),
+				AmountTag: PropTypes.oneOf(['div', 'span']),
+				skuCode: PropTypes.string,
+				className: PropTypes.string,
+				AmountProps: PropTypes.shape({
+						amount: PropTypes.shape({className: PropTypes.string}),
+						compare: PropTypes.shape({className: PropTypes.string})
+				})
+		}
 
-	static defaultProps = {
-		ContainerTag: 'div',
-		AmountTag: 'span'
-	}
+		static defaultProps = {
+				ContainerTag: 'div',
+				AmountTag: 'span',
+				AmountProps: {
+						amount: {
+								className: ''
+						},
+						compare: {
+								className: ''
+						}
+				}
+		}
 
-	render() {
-		const { ContainerTag, AmountTag, skuCode, className } = this.props
+		render() {
+				const {
+						ContainerTag,
+						AmountTag,
+						skuCode,
+						className,
+						AmountProps: {
+								amount,
+								compare
+						}
+				} = this.props
 
-		return (
-			<ContainerTag
-				className={`clayer-price ${className}`}
-				data-sku-code={skuCode}
-			>
-				<AmountTag className='amount' />
-				<AmountTag className='compare-at-amount' />
-			</ContainerTag>
-		)
-	}
+				return (
+						<ContainerTag className={`clayer-price ${className}`} data-sku-code={skuCode}>
+								<AmountTag className={`amount ${amount.className}`}/>
+								<AmountTag className={`compare-at-amount ${compare.className}`}/>
+						</ContainerTag>
+				)
+		}
 }
